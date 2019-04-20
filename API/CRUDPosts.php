1f916266f5e17e51cb2310634379ba7bee20e813
Tarek Sherif <Eng.tarek.sherif@gmail.com>
2019-04-20 16:40:16 +0200

    AddPost

--- API/CRUDPosts.php
+++ API/CRUDPosts.php
@@ -2,6 +2,38 @@
 /**
  * Created by PhpStorm.
  * User: ramy
- * Date: 4/20/19
- * Time: 4:16 PM
+ * Date: 4/19/19
+ * Time: 5:35 PM
  */
+
+// $app->post('/AddPost/{username}',function (\Slim\Http\Request $request,\Slim\Http\Response $response,$args){
+
+//      $name=$args['username'];
+//      $pass=$args['password'];
+
+
+//       $query=new $this->ParseQuery('SlimPosts');
+//       $query->equalTo('name',$name);
+//       $query->equalTo('password',$pass);
+//       $user= $query->find();
+//       if(isset($user[0])){
+//          return $response->withJson(['status'=>true,'login'=>true],200);
+//     }
+//      return $response->withJson(['status'=>false,'login'=>false],401);
+
+// });
+
+
+$app->post('/AddPost',function (\Slim\Http\Request $request ,\Slim\Http\Response $response){
+
+     $data=$request->getParsedBody();
+
+     $newPost=new $this->ParseObject('SlimPosts');
+     $newPost->set('title',$data['title']);
+     $newPost->set('body',$data['body']);
+     $newPost->set('likes',$data['likes']);
+     $newPost->save();
+     return $response->withJson(['status'=>true,'PostId'=>$newPost->getObjectId()]);
+
+
+});
