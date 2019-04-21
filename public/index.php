7b669c22d4972cea39e887a0f50a52a299e6fedf
Ramy Hakam <pencilsoft1@gmail.com>
2019-04-21 07:39:53 +0200

    CRUD post

--- public/index.php
+++ public/index.php
@@ -24,7 +24,7 @@ require __DIR__ . '/../Config/dependiancies.php';
  require '../raouf/raouf.php';
  require '../TarekSherif/TarekSherif.php';
  require  '../API/login.php';
- //require "../API/CRUDPosts.php";
+ require "../API/CRUDPosts.php";
  require '../raouf/CRUDPosts.php';
 
 
